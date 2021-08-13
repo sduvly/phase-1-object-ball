e40da2edcc8e3ad257a419e51f2766b8d15d2133
sduvly <sduvly@gmail.com>
2021-08-13 11:43:35 -0400

    away stats

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -4,7 +4,7 @@ function gameObject(){
           colors:  []
           players:{
               "Alan Anderson": { "number": 0, "shoe": 16, "points": 22, "rebounds": 12, "assist": 12, "steals": 3, "blocks": 1, "slamDunks": 1}
-              "Reggie Evans": { "number": 30, "shoe": 14, "points" 12:, "rebounds": 12, "assist": 12, "steals": 12, "blocks": 12, "slamDunks" : 7}
+              "Reggie Evans": { "number": 30, "shoe": 14, "points": 12, "rebounds": 12, "assist": 12, "steals": 12, "blocks": 12, "slamDunks" : 7}
               "Brook Lopez": { "number": 11, "shoe": 17, "points": 17, "rebounds": 19, "assist": 10, "steals": 3, "blocks": 1, "slamDunks" : 15}
               "Mason Plumlee": { "number": 1, "shoe": 19, "points": 26, "rebounds": 12, "assist": 6, "steals": 3, "blocks": 8, "slamDunks" : 5}
               "Jason Terry": { "number": 31, "shoe": 15, "points": 19, "rebounds": 2, "assist": 2, "steals": 4, "blocks": 11, "slamDunks" : 1}
@@ -12,6 +12,18 @@ function gameObject(){
           }
    }
    away:{
+       teamName: " "
+       colors: []
+       players: {
+           "jeff Adrien":{ "number": 4, "shoe": 18, "points": 10, "rebounds": 1, "assist": 1, "steals": 2, "blocks": 7, "slamDunks": 2}
+           "Bismak Biyombo": {{ "number": 0, "shoe": 16, "points": 12, "rebounds": 4, "assist": 7, "steals": 7, "blocks": 15, "slamDunks": 10}
+           "Desagna Diop": { "number": 2, "shoe": 14, "points": 24, "rebounds": 12, "assist": 12, "steals": 4, "blocks": 5, "slamDunks": 5}
+           "Ben Gordon": { "number": 8, "shoe": 15, "points": 33, "rebounds": 3, "assist": 2, "steals": 1, "blocks": 1, "slamDunks": 0}
+           "Bredan Haywood": { "number": 33, "shoe": 15, "points": 6, "rebounds": 12, "assist": 12, "steals": 22, "blocks": 5, "slamDunks": 12}
+
+
+        }
+       }
 
    }
 }
